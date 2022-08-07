7cc50e4b6c070ea6c2ab62e4d51323d592c26639
deankp <dperez0689@gmail.com>
2022-08-07 13:48:44 -0500

    added stages_controller

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 const express = require("express");
 const app = express();
 const { Sequelize } = require("sequelize");
+const bands = require("./controllers/bands_controller");
 
 // CONFIGURATION / MIDDLEWARE
 require("dotenv").config();
@@ -19,7 +20,13 @@ app.get("/", (req, res) => {
 const bandsController = require("./controllers/bands_controller");
 app.use("/bands", bandsController);
 
+const eventsController = require("./controllers/events_controller");
+app.use("/events", eventsController);
+
+const stagesController = require("./controllers/stages_controller");
+app.use("/stages", stagesController);
+
 // LISTEN
 app.listen(process.env.PORT, () => {
-  console.log(`🎸 Rockin' on port: ${process.env.PG_URI}`);
+  console.log(`🎸 Rockin' on port: ${process.env.PORT}`);
 });
